2dc61076c8ed56b3e786ab74b940a4ca3b4542dc
Thosara Karunathilaka <88446551+Thosara-Yethmal@users.noreply.github.com>
2023-02-03 23:01:54 +0530

    Update index.php
    Make a footer changers

--- sm/school/index.php
+++ sm/school/index.php
@@ -453,13 +453,10 @@
 
     <div class="container mt-4">
       <div class="copyright">
-        &copy; Copyright <strong><span>Impact</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>School.lk</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/impact-bootstrap-business-website-template/ -->
+
         Designed by <a href="">Thosara Karunathilaka</a>
       </div>
     </div>
